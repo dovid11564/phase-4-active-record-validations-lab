5ecb8306b60091b14507ae8a055ea2406b4e9c56
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-12-19 13:48:34 -0500

    commit tree df8b39ae0edc1832b813ce2ec2b317e8323fd785

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true 
+    validates :name, confirmation: { case_sensitive: true}
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
